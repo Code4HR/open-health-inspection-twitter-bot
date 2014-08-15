d2dd1dde7727092b54d9c7da419fc172e5a86081
Blaine Price <1wbprice@gmail.com>
2014-08-15 18:55:01 -0400

    add city reference and http modules.

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 var Twit = require('twit');
+var http = require('http');
+var cities = require('libs/cityReference.js');
 
 var T = new Twit({
 	consumer_key: 'LGQ1D4UMadd5kEgozln4fqDBm',
@@ -6,3 +8,4 @@ var T = new Twit({
   access_token: '2663087029-IbzQFeaS3VmSVs0rWmvvRFbgEYyQsbIHpTKemqM',
   access_token_secret: 'nlGP1D7hFZ1a1tbw4BYMaiMZdLgQLQBD1DakXWVyEU1Lf'
 });
+
