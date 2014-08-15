18e65072652bf94f23d9d5e0b18a5c5b522ef2f8
Blaine Price <blaine@Blaines-MacBook-Pro.local>
2014-08-15 18:45:02 -0400

    add bot.js and keys.

---
+++ bot.js
@@ -0,0 +1,8 @@
+var Twit = require('twit');
+
+var T = new Twit({
+	consumer_key: 'LGQ1D4UMadd5kEgozln4fqDBm',
+  consumer_secret: 'ZfmLkNKTm06LdNcD0wGijkS9akhT0obgeS2PlM1BDajS9rUtOB',
+  access_token: '2663087029-IbzQFeaS3VmSVs0rWmvvRFbgEYyQsbIHpTKemqM',
+  access_token_secret: 'nlGP1D7hFZ1a1tbw4BYMaiMZdLgQLQBD1DakXWVyEU1Lf'
+});
